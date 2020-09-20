a4c247e1c7846bdf9e47738194ca26396177b7a7
jd41 <65656628+jd41@users.noreply.github.com>
2020-09-20 11:56:28 +0200

    exclude pattern for glossary_include.rst

--- doc/conf.py
+++ doc/conf.py
@@ -147,7 +147,7 @@ language = None
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'conngen',
-                    'nest_by_example', 'README.md']
+                    'nest_by_example', 'README.md', 'glossary_include.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'manni'
